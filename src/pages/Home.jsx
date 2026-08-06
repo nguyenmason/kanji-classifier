d084b10831ce33d41b4e42277d23b30e3d91b71e
nguyenmason <143422790+nguyenmason@users.noreply.github.com>
2026-08-06 14:32:53 -0500

    canvas implemented pt 2

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,15 @@
 import './Home.css';
 import Banner from '../components/Banner';
 import Footer from '../components/Footer';
+import Canvas from '../components/Canvas';
 
 export default function Home() {
     return (
         <>
             <Banner currentPage='home' />
-            <div class="body">
-                <div style={{height: 1000}}>
-                    <h1>Hello World!</h1>
-                </div>
-            </div>
+            <main>
+                <Canvas/>
+            </main>
             <Footer />
         </>
     );
