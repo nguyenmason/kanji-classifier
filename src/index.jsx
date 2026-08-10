57665a655c3dc8b950ec745047c390fb91572de3
nguyenmason <143422790+nguyenmason@users.noreply.github.com>
2026-08-10 17:42:24 -0500

    migrating to vercel so no more relative paths

--- src/index.jsx
+++ src/index.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <BrowserRouter basename='/kanji-classifier/'>
+    <BrowserRouter basename='/'>
         <Routes>
             <Route path='/' element={<Home/>}/>
         </Routes>
